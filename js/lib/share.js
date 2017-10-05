44f14c48ef6a8391318f7a31580b76977e012ca6
mikl778890@yandex.ru <adminadmin123>
2017-10-05 16:05:44 +0300

    add google plus to social btn

--- js/lib/share.js
+++ js/lib/share.js
@@ -24,6 +24,11 @@ Share = {
         url += 'url=' + encodeURIComponent(purl);
         Share.popup(url)
     },
+    google: function (purl) {
+        url = 'https://plus.google.com/share?';
+        url += 'url=' + encodeURIComponent(purl);
+        Share.popup(url)
+    },
 
     popup: function (url) {
         window.open(url, '', 'toolbar=0,status=0,width=626,height=436');
